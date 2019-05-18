f2cbff0016b5894bb45497c82d7288414f9442a1
Anthony Rurup <tcrurup@gmail.com>
2019-05-18 02:30:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
       FROM dogs
       WHERE dogs.id = ?
     SQL
-    result = DB[:conn].prepare(sql).execute(id)
+    result = DB[:conn].prepare(sql).execute(id)[0]
     self.new(id: result[0], name: result[1], breed: result[2])
   end
     
