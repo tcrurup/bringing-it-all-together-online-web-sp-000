9cc5e448c2add9467178d690f0b668765815c791
Anthony Rurup <tcrurup@gmail.com>
2019-05-18 02:16:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,7 +11,9 @@ class Dog
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs(
-        id INTEGER PRIMARY KEY
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
       )
     SQL
   end
